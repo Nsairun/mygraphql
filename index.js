472840e5bfdf84265b12f64c2079b86bb00b0285
Nsairun <emmansairun@gmail.com>
2024-02-06 18:45:48 +0100

    creating queries

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import { ApolloServer } from "@apollo/server"; //server that handles the various requests
 import {startStandaloneServer} from "@apollo/server/standalone";// where our data will run or start
 
+// import schema to use type in the server
+import { typedefs } from "./schema";
+
+// import types
+import db from './_db';
+
+const resolvers = {
+    Query:{
+        games(){
+            return db.games
+        },
+        reviews(){
+            return db.games
+        },
+        authors(){
+            return db.authors
+        }
+    }
+}
+
 // server setup
 
 const server = new ApolloServer({
-//typedefs
-//resolvers
+typedefs,
+resolvers
 })
 
 const {url} = await startStandaloneServer(server, {listen: {port:4000}})
--- schema.js
+++ schema.js
@@ -3,5 +3,22 @@ export const typedefs =`#graphql
 type Game {
  id: ID!
  title: String!
- 
-}`
+ platform: String!
+}
+
+type Review {
+    id: ID!
+    rating: Int!
+    content: string!
+}
+type Author {
+    id: ID!
+    name: string!
+    verified: Boolean!
+}
+type Query {
+    reviews: [Review]
+    games: [Game]
+    authors:[Author]
+}
+`
