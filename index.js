1dc2850a1f43ddb999a113be42b3f03b5907c9a3
Nsairun <emmansairun@gmail.com>
2024-02-26 12:05:42 +0100

    working on mutation of data

--- index.js
+++ index.js
@@ -1,33 +1,74 @@
 import { ApolloServer } from "@apollo/server"; //server that handles the various requests
-import {startStandaloneServer} from "@apollo/server/standalone";// where our data will run or start
-
+import { startStandaloneServer } from "@apollo/server/standalone"; // where our data will run or start
 // import schema to use type in the server
-import { typedefs } from "./schema";
+import { typeDefs } from "./schema.js";
 
 // import types
-import db from './_db';
+import db from "./_db.js";
 
 const resolvers = {
-    Query:{
-        games(){
-            return db.games
-        },
-        reviews(){
-            return db.games
-        },
-        authors(){
-            return db.authors
+  Query: {
+    games() {
+      return db.games;
+    },
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      returndb.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      returndb.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    games(parent) {
+      returndb.games.find((g) => g.id === parent.id);
+    },
+    authors(parent) {
+      returndb.authors.find((a) => a.id === parent.id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((g) => g.id !== args.id)
+      return db.games
+    },
+    addGame(_, args){ 
+        let game = {
+            ...args.game,
+            id: Math.floor(Math.random() *10000).toString()
         }
+        db.games.push(game)
+        return game
     }
-}
+  },
+
+};
 
 // server setup
 
 const server = new ApolloServer({
-typedefs,
-resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-const {url} = await startStandaloneServer(server, {listen: {port:4000}})
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
-console.log('server ready to run at port', 4000)
+console.log("server ready to run at port", 4000);
--- schema.js
+++ schema.js
@@ -1,24 +1,38 @@
-// where will define all the different types of data
-export const typedefs =`#graphql 
+// where we define all the different types of data
+export const typeDefs = `#graphql 
 type Game {
- id: ID!
- title: String!
- platform: String!
+  id: ID!
+  title: String!
+  platform: [String!]!
+  reviews: [Review!]
 }
-
 type Review {
-    id: ID!
-    rating: Int!
-    content: string!
+  id: ID!
+  rating: Int!
+  content: String!
+  games: Game!
+  authors: [Author!]
 }
 type Author {
-    id: ID!
-    name: string!
-    verified: Boolean!
+  id: ID!
+  name: String!
+  verified: Boolean!
+  reviews: [Review!]
 }
 type Query {
-    reviews: [Review]
-    games: [Game]
-    authors:[Author]
+  reviews: [Review]
+  review(id:ID): Review
+  games: [Game]
+  game(id:ID): Game
+  authors: [Author]
+  author(id: ID): Author
+}
+type Mutation {
+  deleteGame(id:ID!): [Game]
+  addGame(game: AddGameInput!): Game
+}
+input AddGameInput {
+  title: String!,
+  platform: [String!]!
 }
 `
